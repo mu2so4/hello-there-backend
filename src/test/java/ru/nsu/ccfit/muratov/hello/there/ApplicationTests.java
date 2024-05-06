f8b9edc3afc3aef809e6e664c8d83b5686f62ee1
mu2so4 <b4.orangutang@gmail.com>
2024-05-06 23:57:58 +0700

    Initial commit

---
+++ src/test/java/ru/nsu/ccfit/muratov/hello/there/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.ccfit.muratov.hello.there;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
