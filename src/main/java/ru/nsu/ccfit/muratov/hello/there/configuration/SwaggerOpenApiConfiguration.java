06061ee25951b3fa332b0cc148593fee1984eff1
mu2so4 <b4.orangutang@gmail.com>
2024-06-09 14:04:15 +0700

    Add authentication in Swagger UI

---
+++ src/main/java/ru/nsu/ccfit/muratov/hello/there/configuration/SwaggerOpenApiConfiguration.java
@@ -0,0 +1,28 @@
+package ru.nsu.ccfit.muratov.hello.there.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerOpenApiConfiguration {
+    @Bean
+    public OpenAPI springOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("Hello there API")
+                        .description("Hello there social media")
+                        .version("v0.0.1"));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
