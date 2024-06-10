9403c704e9983115e8f5dc0323e46041f900e580
mu2so4 <b4.orangutang@gmail.com>
2024-06-10 16:02:34 +0700

    Add user repository create test

--- src/test/java/ru/nsu/ccfit/muratov/hello/there/repository/UserRepositoryTest.java
+++ src/test/java/ru/nsu/ccfit/muratov/hello/there/repository/UserRepositoryTest.java
@@ -1,21 +1,49 @@
 package ru.nsu.ccfit.muratov.hello.there.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import ru.nsu.ccfit.muratov.hello.there.entity.Role;
 import ru.nsu.ccfit.muratov.hello.there.entity.UserEntity;
 
-import java.util.Optional;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Set;
 
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class UserRepositoryTest {
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
+
+    private static UserEntity createDummyUser() throws ParseException {
+        Role role = new Role();
+        role.setName("USER");
+        UserEntity user = new UserEntity();
+        user.setUsername("mu2so4");
+        user.setPassword("1234");
+        user.setRoles(Set.of(role));
+        user.setFirstName("Maxim");
+        user.setLastName("Muratov");
+        SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = parser.parse("2002-01-01");
+        user.setBirthday(date);
+        user.setRegistrationTime(new Date());
+        return user;
+    }
 
     @Test
-    public void loadByUsername() {
-        Optional<UserEntity> users = repository.findByUsername("mu2so4");
-        System.out.println(users.orElseThrow(() -> new UsernameNotFoundException("user not found")));
+    public void createUser_thenSuccess() throws ParseException {
+        UserEntity user = createDummyUser();
+
+        UserEntity savedUser = userRepository.save(user);
+
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+        Assertions.assertThat(savedUser).isEqualTo(user);
     }
 }
