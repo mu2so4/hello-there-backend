41810f4f88a785839309cf3526828fed6d12d6c4
mu2so4 <b4.orangutang@gmail.com>
2024-06-12 17:54:25 +0700

    Refactor AuthenticationController

--- src/main/java/ru/nsu/ccfit/muratov/hello/there/controller/AuthenticationController.java
+++ src/main/java/ru/nsu/ccfit/muratov/hello/there/controller/AuthenticationController.java
@@ -8,11 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import ru.nsu.ccfit.muratov.hello.there.security.JwtAuthFilter;
@@ -31,9 +27,6 @@ import java.util.logging.Logger;
 @Tag(name = "Authentication")
 public class AuthenticationController {
     @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
     private JwtService jwtService;
     @Autowired
     private TokenBlacklist tokenBlacklist;
@@ -43,20 +36,12 @@ public class AuthenticationController {
 
     private static final Logger logger = Logger.getLogger(AuthenticationController.class.getCanonicalName());
 
-    @Operation(
-            summary = "Register a new user",
+    @Operation(summary = "Register a new user",
             description = "Registers a new user in the system."
     )
     @ApiResponses({
-            @ApiResponse(
-                    description = "Success",
-                    responseCode = "201"
-            ),
-            @ApiResponse(
-                    description = "Invalid form",
-                    responseCode = "400",
-                    content = @Content
-            )
+            @ApiResponse(description = "Success", responseCode = "201"),
+            @ApiResponse(description = "Invalid form", responseCode = "400", content = @Content)
     })
     @PostMapping(value = "/register", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
@@ -64,52 +49,31 @@ public class AuthenticationController {
         return new RegistrationResponseDto(userEntityService.registerUser(form));
     }
 
-    @Operation(
-            summary = "Sign in to the system"
-    )
+    @Operation(summary = "Sign in to the system")
     @ApiResponses({
-            @ApiResponse(
-                    description = "Success",
-                    responseCode = "201"
-            ),
-            @ApiResponse(
-                    description = "Invalid credentials",
-                    responseCode = "400",
-                    content = @Content
-            )
+            @ApiResponse(description = "Success", responseCode = "201"),
+            @ApiResponse(description = "Invalid credentials", responseCode = "400", content = @Content)
     })
     @PostMapping(value = "/login", produces = "application/json")
     @ResponseStatus(code = HttpStatus.CREATED)
     public AuthResponseDto login(@RequestBody LoginDto dto) {
         String username = dto.getUsername();
         logger.info(() -> String.format("received login request for user \"%s\"", username));
-        Authentication authentication;
         try {
-            authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(username, dto.getPassword()));
+            String token = jwtService.login(dto);
+            return new AuthResponseDto(token);
         }
         catch(AuthenticationException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String token = jwtService.generateToken(authentication);
-        return new AuthResponseDto(token);
     }
 
-    @Operation(
-            summary = "Logout user",
+    @Operation(summary = "Logout user",
             description = "Invalidates their JWT"
     )
     @ApiResponses({
-            @ApiResponse(
-                    description = "Success",
-                    responseCode = "204"
-            ),
-            @ApiResponse(
-                    description = "Invalid credentials",
-                    responseCode = "401",
-                    content = @Content
-            )
+            @ApiResponse(description = "Success", responseCode = "204"),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content)
     })
     @DeleteMapping(value = "/logout")
     public void logout(HttpServletRequest request) {
