5c5f1c673f86f480be73bc1164b48e85a6d2af41
mu2so4 <b4.orangutang@gmail.com>
2024-06-13 15:39:05 +0700

    Inject real passwordEncoder

--- src/test/java/ru/nsu/ccfit/muratov/hello/there/service/UserServiceTest.java
+++ src/test/java/ru/nsu/ccfit/muratov/hello/there/service/UserServiceTest.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.nsu.ccfit.muratov.hello.there.dto.auth.RegistrationRequestDto;
 import ru.nsu.ccfit.muratov.hello.there.entity.Role;
 import ru.nsu.ccfit.muratov.hello.there.entity.UserEntity;
@@ -27,53 +27,61 @@ import java.util.Optional;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@ExtendWith({MockitoExtension.class, SpringExtension.class})
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
     @Mock
     private UserRepository userRepository;
     @Mock
     private RoleRepository roleRepository;
-    @Mock
+
     private PasswordEncoder passwordEncoder;
 
     @InjectMocks
     private UserEntityServiceImpl userService;
 
-    @Test
-    @DisplayName("Create new user")
-    public void create() throws ParseException {
-        final String ROLE_NAME = "USER";
-        Role role = new Role(1, ROLE_NAME);
-        Integer userId = 1234;
-        when(userRepository.save(any(UserEntity.class))).thenAnswer((invocation) -> {
-            UserEntity savedUser = invocation.getArgument(0, UserEntity.class);
-            savedUser.setId(userId);
-            return savedUser;
-        });
-        when(passwordEncoder.encode(any(CharSequence.class)))
-                .then((invocation) -> invocation.getArgument(0, String.class));
-        when(roleRepository.findByName("USER")).thenReturn(role);
-        RegistrationRequestDto dto = new RegistrationRequestDto();
-        dto.setUsername("mu2so4");
-        dto.setPassword("1234");
-        dto.setFirstName("Maxim");
-        dto.setLastName("Muratov");
-        SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd");
-        Date date = parser.parse("2002-01-01");
-        dto.setBirthday(date);
+    @Nested
+    public class RegisterUserTest {
+        @BeforeEach
+        public void init() {
+            passwordEncoder = new BCryptPasswordEncoder();
+            userService = new UserEntityServiceImpl(userRepository, null, roleRepository, passwordEncoder);
+        }
 
-        UserEntity user = userService.registerUser(dto);
+        @Test
+        @DisplayName("Create new user")
+        public void create() throws ParseException {
+            final String ROLE_NAME = "USER";
+            Role role = new Role(1, ROLE_NAME);
+            Integer userId = 1234;
+            when(userRepository.save(any(UserEntity.class))).thenAnswer((invocation) -> {
+                UserEntity savedUser = invocation.getArgument(0, UserEntity.class);
+                savedUser.setId(userId);
+                return savedUser;
+            });
+            when(roleRepository.findByName("USER")).thenReturn(role);
+            RegistrationRequestDto dto = new RegistrationRequestDto();
+            dto.setUsername("mu2so4");
+            dto.setPassword("1234");
+            dto.setFirstName("Maxim");
+            dto.setLastName("Muratov");
+            SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = parser.parse("2002-01-01");
+            dto.setBirthday(date);
 
-        Assertions.assertThat(user).isNotNull();
-        Assertions.assertThat(user.getId()).isEqualTo(userId);
-        Assertions.assertThat(user.getUsername()).isEqualTo(dto.getUsername());
-        Assertions.assertThat(user.getFirstName()).isEqualTo(dto.getFirstName());
-        Assertions.assertThat(user.getLastName()).isEqualTo(dto.getLastName());
-        Assertions.assertThat(user.getBirthday()).isEqualTo(dto.getBirthday());
-        Assertions.assertThat(user.getPassword()).isEqualTo(dto.getPassword()); //fixme
-        Assertions.assertThat(user.getRoles())
-                .contains(role)
-                .hasSize(1);
+            UserEntity user = userService.registerUser(dto);
+
+            Assertions.assertThat(user).isNotNull();
+            Assertions.assertThat(user.getId()).isEqualTo(userId);
+            Assertions.assertThat(user.getUsername()).isEqualTo(dto.getUsername());
+            Assertions.assertThat(user.getFirstName()).isEqualTo(dto.getFirstName());
+            Assertions.assertThat(user.getLastName()).isEqualTo(dto.getLastName());
+            Assertions.assertThat(user.getBirthday()).isEqualTo(dto.getBirthday());
+            Assertions.assertThat(passwordEncoder.matches(dto.getPassword(), user.getPassword()))
+                    .isTrue();
+            Assertions.assertThat(user.getRoles())
+                    .contains(role)
+                    .hasSize(1);
+        }
     }
 
     @Nested
